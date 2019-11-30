09d72ecfe523a6d8e6ae80a590b2844457efefb8
Michael Coyne <mikeycgto@gmail.com>
2019-11-30 13:32:28 -0500

    chore: update janus script

--- janus.sh
+++ janus.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd ~/.janus
+
 git clone https://github.com/godlygeek/tabular.git
 git clone https://github.com/qpkorr/vim-bufkill.git
 git clone https://github.com/styled-components/vim-styled-components.git
